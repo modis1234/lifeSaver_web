9c3b15a0537ddcedcf2a78a4af868f2a7dcedfc5
admin <modis1234@openworks.co.kr>
2020-11-09 16:25:42 +0900

    운영환경

--- routes/config/connection.js
+++ routes/config/connection.js
@@ -3,7 +3,7 @@ var dbconfig = require('./database');
 
 //var conn = mysql.createConnection(dbconfig.operation);
 //풀에서 컨넥션 생성
-var pool = mysql.createPool(dbconfig.develop);
+var pool = mysql.createPool(dbconfig.operation);
 
 
 
