068d500646e293554ee12e957488baa6fc7cac17
admin <modis1234@openworks.co.kr>
2020-11-16 14:41:07 +0900

    11/16 database.js 변뎡

--- routes/config/database.js
+++ routes/config/database.js
@@ -5,26 +5,24 @@ var _mysql = {
     user: 'root',
     password: 'work1801!',
     database: 'life_saver_gas',
-    multipleStatements: true, //다중 퀄리 사용가능
-    connectionLimit: 200
+    multipleStatements: true //다중 퀄리 사용가능
   },
+  // operation: {
+  //   host: '127.0.0.1',
+  //   port: '3306',
+  //   user: 'root',
+  //   password: 'work1801',
+  //   database: 'life_saver_gas',
+  //   multipleStatements: true //다중 퀄리 사용가능
+  // },
   operation: {
-    host: '127.0.0.1',
+    host: '119.207.78.146',
     port: '3306',
-    user: 'root',
+    user: 'admin',
     password: 'work1801',
     database: 'life_saver_gas',
     multipleStatements: true //다중 퀄리 사용가능
   }
-  // operation: {
-  //   host: '119.207.78.146',
-  //   port: '3306',
-  //   user: 'admin',
-  //   password: 'work1801',
-  //   database: 'life_saver_gas',
-  //   multipleStatements: true, //다중 퀄리 사용가능
-  //   connectionLimit: 200
-  // }
 }
 
 module.exports = _mysql
